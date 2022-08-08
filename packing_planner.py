7e3fdff49c749f9b1c3548d964e9133f361f85f8
Jay Palat <jay.palat@gmail.com>
2022-08-08 06:37:35 -0400

    Created subparsers for inventory, trip and plan.
    Inventory will manage item to pack.  Plan is reusable plan or template
    for future trips.  Trip is a specific instance of a plan.

--- packing_planner.py
+++ packing_planner.py
@@ -6,17 +6,24 @@ import sys
 
 #create the parser
 parser = argparse.ArgumentParser()
-parser.add_argument('Path', 
-        metavar='path', 
-        type=str, 
-        help='path to list')
+
+subparsers = parser.add_subparsers()
+parser_inv = subparsers.add_parser('inventory', help='inventory help')
+sub_inventory = parser_inv.add_subparsers()
+si_add = sub_inventory.add_parser('add', help='Add inventory')
+si_add.add_argument('item')
+
+
+parser_plan = subparsers.add_parser('plan', help='plan help')
+parser_plan.add_argument('--name', type=str, help='name of the plan')
+
+parser_trip = subparsers.add_parser('trip', help='trip help')
+parser_trip.add_argument('--destination', help='trip destination.')
+parser_trip.add_argument('--start_date', help='trip start.')
+
+
 
 args = parser.parse_args()
 
-input_path = args.Path
 
-if not os.path.isdir(input_path):
-    print('The path specified does not exist')
-    sys.exit()
-
-print('\n'.join(os.listdir(input_path)))
+print(args)
