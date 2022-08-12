00250354c1260733421eb013f9e42dec93b18b36
Jay Palat <jay.palat@gmail.com>
2022-08-12 18:51:32 -0400

    Update naming for install purposes.

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 setup(
-    name = 'packplannner',
+    name = 'packing_planner',
     version = '0.1.0',
     packages = ['packing_planner'],
     entry_points = {
         'console_scripts': [
-            'packplannner = packing_plannner.__main__:main'
+            'packing_planner = packing_planner.cli:main'
         ]
+
     })
