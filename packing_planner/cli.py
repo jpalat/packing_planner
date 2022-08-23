fc5ed0e9790e2aa43fd7487f97cd017e2f44d09b
Jay Palat <vpalat@sei.cmu.edu>
2022-08-23 11:22:00 -0400

    Add more functions to Inventory
    Serialize and deserialize the inventory list.
    Add and print from the CLI.

--- packing_planner/cli.py
+++ packing_planner/cli.py
@@ -11,14 +11,15 @@ def main():
     #create the parser
     parser = argparse.ArgumentParser()
     inventory = Inventory()
-    inventory.loadInventory()
 
     subparsers = parser.add_subparsers()
     parser_inv = subparsers.add_parser('inventory', help='inventory help')
+    # parser_inv.add_argument('list', help='list inventory', action='store_true')
     sub_inventory = parser_inv.add_subparsers()
     si_add = sub_inventory.add_parser('add', help='Add inventory')
     si_add.add_argument('item')
-
+    si_list = sub_inventory.add_parser('list', help='list inventory')
+    si_list.add_argument(dest='list', action='store_true')
 
     parser_plan = subparsers.add_parser('plan', help='plan help')
     parser_plan.add_argument('--name', type=str, help='name of the plan')
@@ -27,17 +28,17 @@ def main():
     parser_trip.add_argument('--destination', help='trip destination.')
     parser_trip.add_argument('--start_date', help='trip start.')
 
-
-
     args = parser.parse_args()
 
     arg_dict = vars(args)
     print(arg_dict)
 
-    if arg_dict['item']:
+    if 'item' in arg_dict:
         print("Adding inventory item")
         inventory.addItem(arg_dict['item'])
         inventory.saveInventory()
+    if 'list' in arg_dict:
+        inventory.printInventory()
 
 
 if __name__ == '__main__':
--- packing_planner/inventory.py
+++ packing_planner/inventory.py
@@ -1,3 +1,6 @@
+import pickle
+import os.path
+
 class Item:
     def __init__(self) -> None:
         pass
@@ -6,17 +9,39 @@ class Item:
 
 class Inventory:
     inventory = []
+
+    def __init__(self) -> None:
+        self.filename = '.pplan_inv.pkl'
+        if os.stat(self.filename).st_size == 0:
+            print('File is empty')
+        else:
+            self.loadInventory()
+                
     
     def addItem(self, item):
         self.inventory.append(item)
     
     def loadInventory(self):
         print("Load Inventory")
+        inputFile = open(self.filename, 'rb')
+        self.inventory = pickle.load(inputFile)        
+        inputFile.close()
 
     def saveInventory(self):
         print("Save Inventory")
+        pp = []
         for i in self.inventory:
             print(i)
+            pp.append(i)
+        outputFile = open(self.filename, 'wb')
+        pickle.dump(pp, outputFile)
+        outputFile.close()
+
+    def printInventory(self):
+        print("Print Inventory")
+        for i in self.inventory:
+            print(i)
+
 
     def removeItem(self, index):
         print("Remove item at Index" + index)
